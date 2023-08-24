65584459cf068c6239238c9bc88991b79cc621fe
Clarice Yekeh <clariceyekeh@gmail.com>
2023-08-24 11:48:22 -0700

    subplots

--- data_analysis.py
+++ data_analysis.py
@@ -2,28 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 data = np.loadtxt('HOM_Traces_20220814_1222_51.txt', skiprows=5)
 
-x = data[:, 0]
-y = data[:, 1]
-z = data[:, 2]
-a = data[:, 3]
+ch0 = data[:, 0]
+ch1 = data[:, 1]
+ch2 = data[:, 2]
+ch3 = data[:, 3]
 
-var = [x,y,z,a]
+fig, ax = plt.subplots(1,2)
+var = [ch0,ch1,ch2,ch3]
 
-for v in var:
-    b = np.amax(v)
-    print('Max:', b)
-    print('Min:', np.amin(v))
-    print('Average:', np.average(v))
-    plt.plot(b, '.')
-plt.xlabel("Time")
-plt.ylabel("Scope Traces")
-plt.title("Maximum")
+for i, chx in enumerate(var):
+    arr = np.array(chx)
+    print('Max:', np.amax(arr))
+    print('Min:', np.amin(arr))
+    print('Average:', np.average(chx))
+    ax[0].plot(i, np.amax(arr), '.')
+    ax[1].plot(i, np.amin(arr), '.')
+
+ax[0].set_xlabel("Channel")
+ax[0].set_ylabel("Scope Traces")
+ax[0].set_title('Maximum')
+ax[1].set_xlabel("Channel")
+ax[1].set_ylabel("Scope Traces")
+ax[1].set_title('Minimum')
 plt.show()
 
-for v in var:
-    c = np.amin(v)
-    plt.plot(c, '.')
-plt.xlabel("Time")
-plt.ylabel("Scope Traces")
-plt.title("Minimum")
-plt.show()
